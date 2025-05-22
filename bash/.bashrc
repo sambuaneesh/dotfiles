0372a546dc0c37372b4241daab57519240688909
Aneesh Sambu <sambuaneesh@gmail.com>
2025-05-22 16:55:57 +0530

    from scratch

--- bash/.bashrc
+++ bash/.bashrc
@@ -1,1 +1,34 @@
+# ~/.bashrc
 
+# recomm defaults
+bind "set completion-ignore-case on"
+HISTCONTROL=ignoreboth
+HISTSIZE=10000
+HISTFILESIZE=20000
+shopt -s histappend
+PROMPT_COMMAND='history -a'
+
+#---
+
+# aliases
+## common
+alias ll='ls -alF --color=auto'
+alias la='ls -A --color=auto'
+alias l='ls -CF --color=auto'
+alias grep='grep --color=auto'
+
+## custom
+alias conf='nvim ~/dotfiles/bash/.bashrc && source ~/.bashrc'
+alias aconf='nvim /mnt/c/Users/Aneesh/AppData/Roaming/alacritty/alacritty.yml'
+alias tconf='nvim ~/dotfiles/tmux/.tmux.conf'
+alias nconf='nvim ~/dotfiles/nvim/.config/nvim/init.lua'
+alias n='nvim'
+alias t='tmux'
+
+#---
+
+# startup hooks
+eval "$(starship init bash)"
+eval "$(zoxide init bash)"
+
+#---
---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,46 @@
+-- my recomm defaults
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.smartindent = true
+vim.opt.cursorline = true
+vim.opt.mouse = 'a'
+vim.opt.swapfile = false
+vim.opt.termguicolors = true
+
+-- remappings
+vim.opt.timeoutlen = 100
+vim.api.nvim_set_keymap('n', 'jk', '<Esc>', { noremap=true, silent=true })
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap=true, silent=true })
+vim.api.nvim_set_keymap('v', 'jk', '<Esc>', { noremap=true, silent=true })
+
+vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-s>', '<Esc>:w<CR>a', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-s>', '<Esc>:w<CR>a', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<C-x>', ':q<CR>', { noremap = true, silent = true })
+
+-- bootstrap Lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+-- plugins
+require("lazy").setup({
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+})
+
+
+-- theme
+vim.cmd.colorscheme("catppuccin-mocha")
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
